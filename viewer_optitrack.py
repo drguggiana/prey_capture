6b068ef610253d4c650025c8d84e41fa3bba4abb
Drago <drguggiana@gmail.com>
2019-11-28 20:02:34 +0100

    improved the viewer with some preprocessing, also for two animals

--- viewer_optitrack.py
+++ viewer_optitrack.py
@@ -3,6 +3,8 @@ from tkinter import filedialog
 from tkinter import Tk
 import matplotlib.pyplot as plt
 import numpy as np
+import paths
+from functions_preprocessing import median_discontinuities, nan_large_jumps, interpolate_segments
 # Create Tk root
 root = Tk()
 # Hide the main window
@@ -10,7 +12,7 @@ root.withdraw()
 root.call('wm', 'attributes', '.', '-topmost', True)
 
 # define loading path and select file
-base_path = r'C:\Users\drguggiana\Documents\Motive_test1\etc'
+base_path = paths.motive_path
 file_path = filedialog.askopenfilenames(initialdir=base_path)
 
 
@@ -49,15 +51,47 @@ for animal in file_path:
     ax = fig.add_subplot(111)
     ax.plot(np.diff(timestamp))
 
+    # median the discontinuities
+    filtered_traces = median_discontinuities(target_data, [[1, 3]], 201)
+    # detect if two mice are present. If so plot also
+    if target_data.shape[1] > 11:
+        filtered_traces = median_discontinuities(target_data, [[7, 9]], 201)
+
+    # eliminate discontinuities before interpolating
+    filtered_traces = nan_large_jumps(filtered_traces, [[1, 3]], 0.05, 300)
+    if target_data.shape[1] > 11:
+        filtered_traces = nan_large_jumps(filtered_traces, [[7, 9]], 0.05, 300)
+
+    # interpolate the NaN stretches
+    target_data = interpolate_segments(filtered_traces, np.nan)
+
     # 2D mouse movement
     fig = plt.figure()
     ax = fig.add_subplot(111)
     number_points = None
     ax.plot(target_data[:number_points, 3], target_data[:number_points, 1])
-    plt.gca().invert_xaxis()
 
+    # detect if two mice are present. If so plot also
+    if target_data.shape[1] > 11:
+        ax.plot(target_data[:number_points, 9], target_data[:number_points, 7])
+
+    plt.gca().invert_xaxis()
     # ax.plot(target_data[:number_points,7], target_data[:number_points,9])
     ax.autoscale()
     ax.axis('equal')
+
+    # plot the individual axes
+    fig2 = plt.figure()
+    ax = fig2.add_subplot(411)
+    ax.plot(target_data[:number_points, 3])
+    ax = fig2.add_subplot(412)
+    ax.plot(target_data[:number_points, 1])
+
+    if target_data.shape[1] > 11:
+        ax = fig2.add_subplot(413)
+        ax.plot(target_data[:number_points, 9])
+        ax = fig2.add_subplot(414)
+        ax.plot(target_data[:number_points, 7])
+
 
 plt.show()
