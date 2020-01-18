8191d0645819d5eae9c3578241a5488a7481e9c9
Drago Guggiana <drguggiana@gmail.com>
2020-01-18 02:20:23 -0300

    added tif combine function

--- functions_io.py
+++ functions_io.py
@@ -2,6 +2,8 @@ import numpy as np
 import csv
 from os.path import basename
 import datetime
+from skimage import io
+from skimage.external import tifffile as tif
 
 
 def load_preprocessed(file_path_in):
@@ -52,3 +54,31 @@ def get_file_date(filename):
     """Extract the file date and time from a bonsai filename"""
     file_date = datetime.datetime.strptime(basename(filename)[:18], '%m_%d_%Y_%H_%M_%S')
     return file_date
+
+
+def combine_tif(filenames):
+    """Function to concatenate together several tif files supplied as a list of paths"""
+    # based on https://stackoverflow.com/questions/47182125/how-to-combine-tif-stacks-in-python
+
+    # read the first stack on the list
+    im_1 = io.imread(filenames[0])
+    # if it's 2d, expand 1 dimension
+    if len(im_1.shape) == 2:
+        im_1 = np.expand_dims(im_1, 2)
+        im_1 = np.transpose(im_1, [2, 0, 1])
+    # assemble the output path
+    out_path = filenames[0].replace('.tif', '_CAT.tif')
+    # run through the remaining files
+    for i in range(1, len(filenames)):
+        # load the next file
+        im_n = io.imread(filenames[i])
+        # if it's 2d, expand 1 dimension
+        if len(im_n.shape) == 2:
+            im_n = np.expand_dims(im_n, 2)
+            im_n = np.transpose(im_n, [2, 0, 1])
+        # concatenate it to the previous one
+        im_1 = np.concatenate((im_1, im_n))
+    # save the final stack
+    tif.imsave(out_path, im_1.astype('uint16'), bigtiff=True)
+
+    return out_path
