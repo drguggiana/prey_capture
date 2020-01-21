84953712f378e8ea5ce08aa28fabe7e7d67b7daf
biojibia <drguggiana@gmail.com>
2020-01-21 12:11:57 +0100

    added tif concatenation

--- calcium_runmin1pipe.py
+++ calcium_runmin1pipe.py
@@ -1,4 +1,21 @@
 import subprocess
 import paths
+import functions_io
+from functions_misc import tk_killwindow
+from tkinter import filedialog
 
-min1pipe_process = subprocess.Popen([paths.minpipe_path])
+
+# get rid of the tk main window
+tk_killwindow()
+
+# prompt the user for file selection
+# define the base loading path
+base_path_bonsai = paths.testmini_path
+# select the files to process
+file_path_bonsai = filedialog.askopenfilenames(initialdir=base_path_bonsai, filetypes=(("tif files", "*.tif"), ))
+
+# combine the selected files into a single tif
+out_path = functions_io.combine_tif(file_path_bonsai)
+
+
+# min1pipe_process = subprocess.Popen([paths.minpipe_path])
