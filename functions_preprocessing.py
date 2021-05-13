fixed rescaling and added saving of the rotation matrix

functions_preprocessing.py
@@ -348,8 +348,9 @@ def interpolate_segments(files, target_value):
     return interpolated_traces
 
 
-def interpolate_animals(files, target_values):
-    """Interpolate the mouse NaNs and prolong the cricket ones"""
+def interpolate_animals(files, target_values, ref_corners, corners, distance_threshold=2):
+    """Correct the cricket position"""
+    # TODO: account for when the cricket is under the mouse
     # extract the mouse coordinates
     mouse_columns = [el for el in files.columns if 'mouse' in el]
     mouse_coordinates = files[mouse_columns]
@@ -363,6 +364,13 @@ def interpolate_animals(files, target_values):
     nan_vector = np.any(np.isnan(cricket_coordinates.to_numpy()), axis=1)
     cricket_coordinates.iloc[nan_vector, :] = np.nan
 
+    distance_mouse = \
+        fk.distance_calculation(cricket_coordinates[['cricket_0_x', 'cricket_0_y']].to_numpy(),
+                                mouse_coordinates[['mouse_x', 'mouse_y']].to_numpy())
+    # convert the distance to cm
+    distance_mouse = distance_mouse*(np.abs(ref_corners[0][1] -
+                                            ref_corners[1][1])/np.abs(corners[0][0] - corners[2][0]))
+
     # if the first position is nan, copy the first not-nan position here
     if np.isnan(cricket_coordinates.iloc[0, 0]):
         # find the first not-nan
@@ -374,6 +382,12 @@ def interpolate_animals(files, target_values):
     for col in cricket_coordinates.columns:
         # get the data
         data = cricket_coordinates[col].to_numpy()
+        # get the target column
+        if 'head' in col:
+            target_column = 'mouse_snout_' + col[-1]
+        else:
+            target_column = 'mouse_head_' + col[-1]
+
         # find the target value
         if np.isnan(target_values):
             nan_locations, nan_numbers = label(np.isnan(data))
@@ -384,8 +398,13 @@ def interpolate_animals(files, target_values):
         for segment in np.arange(1, nan_numbers+1):
             # get the start of the segment
             segment_start = np.argwhere(nan_locations == segment).flatten()[0]
-            # replace the segment by the values
-            data[nan_locations == segment] = data[segment_start-1]
+            # select the action depending on distance
+            if distance_mouse[segment_start] < distance_threshold:
+                # replace the segment by the position of the mouse
+                data[nan_locations == segment] = mouse_coordinates.loc[nan_locations == segment, target_column]
+            else:
+                # replace the segment by the last valid value
+                data[nan_locations == segment] = data[segment_start-1]
         # add to the output frame
         cricket_interpolated.loc[:, col] = data
 
@@ -557,7 +576,7 @@ def rescale_pixels(traces, db_data, reference, manual_coordinates=None):
                                                      np.array(reference).astype('float32'))
     # get the new corners
     new_corners = np.concatenate((corner_coordinates, np.ones((corner_coordinates.shape[0], 1))), axis=1)
-    new_corners = np.matmul(perspective_matrix, new_corners.T).T
+    new_corners = np.matmul(new_corners, perspective_matrix.T)
     new_corners = np.array([el[:2] / el[2] for el in new_corners])
 
     # copy the traces
@@ -575,15 +594,20 @@ def rescale_pixels(traces, db_data, reference, manual_coordinates=None):
             # add a vector of ones for the matrix multiplication
             original_data = np.concatenate((original_data, np.ones((original_data.shape[0], 1))), axis=1)
             # transform
-            new_data = np.matmul(perspective_matrix, original_data.T).T
-            # basic scaling for debugging
-            # new_data = original_data*(np.abs(reference[0][1] - reference[1][1])/
-            # np.abs(corner_coordinates[0][0] - corner_coordinates[2][0]))
+            new_data = np.matmul(original_data, perspective_matrix.T)
+            new_data = np.array([el[:2] / el[2] for el in new_data])
+
+            # # basic scaling for debugging
+            # new_data = original_data*(np.abs(reference[0][1] - reference[1][1]) /
+            #                           np.abs(corner_coordinates[0][0] - corner_coordinates[2][0]))
 
             # replace the original data
             new_traces[[column + 'x', column + 'y']] = new_data[:, :2]
 
-    return new_traces, new_corners
+    # turn the perspective matrix into a dataframe
+    output_matrix = pd.DataFrame(perspective_matrix)
+
+    return new_traces, new_corners, output_matrix
 
 
 def find_corners(video_path, num_frames=10, crop_flag=False):

functions_vame.py
@@ -12,6 +12,9 @@ import numpy as np
 import pandas as pd
 import tqdm
 import os
+import h5py
+import matplotlib.pyplot as plt
+import time
 
 
 # Returns cropped image using rect tuple
@@ -21,6 +24,8 @@ def crop_and_flip(rect, src, points, ref_index):
     center, size = tuple(map(int, center)), tuple(map(int, size))
     # Get rotation matrix
     M = cv.getRotationMatrix2D(center, theta, 1)
+    center_movie = tuple([1024-center[0]*1024/1000/40, center[1]*1280/1000/40])
+    M_movie = cv.getRotationMatrix2D(center_movie, theta, 1)
 
     # shift DLC points
     x_diff = center[0] - size[0] // 2
@@ -37,9 +42,10 @@ def crop_and_flip(rect, src, points, ref_index):
         dlc_points_shifted.append(point)
 
     # Perform rotation on src image
-    dst = cv.warpAffine(src.astype('float32'), M, src.shape[:2])
-    out = cv.getRectSubPix(dst, size, center)
-
+    # out = src
+    dst = cv.warpAffine(src.astype('float32'), M_movie, src.shape[:2])
+    out = cv.getRectSubPix(dst, size, center_movie)
+    # print(np.max(out))
     # check if flipped correctly, otherwise flip again
     if dlc_points_shifted[ref_index[1]][0] >= dlc_points_shifted[ref_index[0]][0]:
         rect = ((size[0] // 2, size[0] // 2), size, 180)
@@ -64,8 +70,10 @@ def crop_and_flip(rect, src, points, ref_index):
             dlc_points_shifted.append(point)
 
         # Perform rotation on src image
-        dst = cv.warpAffine(out.astype('float32'), M, out.shape[:2])
-        out = cv.getRectSubPix(dst, size, center)
+        center_movie = tuple([center[0] * 1024 / 1000 / 40, center[1] * 1280 / 1000 / 40])
+        M_movie = cv.getRotationMatrix2D(center_movie, theta, 1)
+        dst = cv.warpAffine(out.astype('float32'), M_movie, out.shape[:2])
+        out = cv.getRectSubPix(dst, size, center_movie)
 
     return out, dlc_points_shifted
 
@@ -89,12 +97,13 @@ def interpol(arr):
     return arr
 
 
-def background(path_to_file, filename, file_format='.mp4', num_frames=1000):
+def background(video_path, num_frames=1000):
     """
     Compute background image from fixed camera
     """
     import scipy.ndimage
-    video_path = os.path.join(path_to_file, 'videos', filename + file_format)
+    # video_path = os.path.join(path_to_file, 'videos', filename + file_format)
+
     capture = cv.VideoCapture(video_path)
 
     if not capture.isOpened():
@@ -106,7 +115,8 @@ def background(path_to_file, filename, file_format='.mp4', num_frames=1000):
     height, width, _ = frame.shape
     frames = np.zeros((height, width, num_frames))
 
-    for i in tqdm.tqdm(range(num_frames), disable=not True, desc='Compute background image for video %s' % filename):
+    for i in tqdm.tqdm(range(num_frames),
+                       disable=not True, desc='Compute background image for video %s' % os.path.basename(video_path)):
         rand = np.random.choice(frame_count, replace=False)
         capture.set(1, rand)
         ret, frame = capture.read()
@@ -124,7 +134,7 @@ def background(path_to_file, filename, file_format='.mp4', num_frames=1000):
 
 
 def align_mouse(path_to_file, filename, file_format, crop_size, pose_list, pose_ref_index,
-                pose_flip_ref, bg, frame_count, use_video=True, interp_flag=True):
+                pose_flip_ref, bg, frame_count, use_video=True, interp_flag=True, vid_path=None):
     # returns: list of cropped images (if video is used) and list of cropped DLC points
     #
     # parameters:
@@ -154,9 +164,12 @@ def align_mouse(path_to_file, filename, file_format, crop_size, pose_list, pose_
 
     if use_video:
         # generate the video path
-        video_path = os.path.join(path_to_file, 'videos', filename + file_format)
-        capture = cv.VideoCapture(video_path)
+        if vid_path is None:
+            video_path = os.path.join(path_to_file, 'videos', filename + file_format)
+        else:
+            video_path = vid_path
         # capture = cv.VideoCapture(path_to_file+'videos/'+filename+file_format)
+        capture = cv.VideoCapture(video_path)
 
         if not capture.isOpened():
             raise Exception("Unable to open video file: {0}".format(video_path))
@@ -203,7 +216,7 @@ def align_mouse(path_to_file, filename, file_format, crop_size, pose_list, pose_
 
         # crop image
         out, shifted_points = crop_and_flip(rect, img, pose_list_bordered, pose_flip_ref)
-
+        # print(np.max(out))
         images.append(out)
         points.append(shifted_points)
 
@@ -220,7 +233,7 @@ def align_mouse(path_to_file, filename, file_format, crop_size, pose_list, pose_
 # play aligned video
 def play_aligned_video(a, n, frame_count):
     colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255),
-              (0, 255, 255), (0, 0, 0), (255, 255, 255)]
+              (0, 255, 255), (0, 0, 0), (255, 255, 255), (127, 0, 127), (0, 127, 127)]
 
     for i in range(frame_count):
         # Capture frame-by-frame
@@ -233,9 +246,14 @@ def play_aligned_video(a, n, frame_count):
             # im_color = frame
 
             for c, j in enumerate(n[i]):
+                j[0] = j[0]*1024/40/1000
+                j[1] = j[1]*1280/40/1000
                 cv.circle(im_color, (j[0], j[1]), 5, colors[c], -1)
 
             cv.imshow('Frame', im_color)
+            time.sleep(0.1)
+            # plt.imshow(frame)
+            # plt.show()
 
             # Press Q on keyboard to  exit
             if cv.waitKey(25) & 0xFF == ord('q'):
@@ -247,7 +265,8 @@ def play_aligned_video(a, n, frame_count):
     cv.destroyAllWindows()
 
 
-def align_demo(path_to_dlc, path_to_file, filename, file_format, crop_size, use_video=False, check_video=False):
+def align_demo(path_to_dlc, path_to_file, filename, file_format,
+               crop_size, use_video=False, check_video=False, vid_path=None):
     # read out data
     # data = pd.read_csv(path_to_file+'videos/pose_estimation/'+filename+'-DC.csv', skiprows = 2)
 
@@ -264,7 +283,12 @@ def align_demo(path_to_dlc, path_to_file, filename, file_format, crop_size, use_
         # set interpolation flag
         interp_flag = True
     else:
-        data = pd.read_hdf(path_to_dlc, 'full_traces')
+        # data = pd.read_hdf(path_to_dlc, 'full_traces')
+        with h5py.File(path_to_dlc, 'r') as f:
+            values = np.array(f['full_traces/block0_values'])
+            labels = np.array(f['full_traces/block0_items']).astype(str)
+            data = pd.DataFrame(values, columns=labels)
+
         # get the column names
         column_list = list(data.columns)
         # # get only the columns with mouse information
@@ -300,12 +324,17 @@ def align_demo(path_to_dlc, path_to_file, filename, file_format, crop_size, use_
     pose_ref_index = [0, 7]
 
     # list of 2 reference coordinate indices for avoiding flipping
-    pose_flip_ref = [1, 2]
+    pose_flip_ref = [0, 7]
 
     if use_video:
-        # compute background
-        bg = background(path_to_file, filename, file_format=file_format)
-        video_path = os.path.join(path_to_file, 'videos', filename + file_format)
+        if vid_path is None:
+            video_path = os.path.join(path_to_file, 'videos', filename + file_format)
+            bg = background(video_path)
+        else:
+            video_path = vid_path
+            # compute background
+            # bg = background(video_path)
+            bg = 0
         capture = cv.VideoCapture(video_path)
         if not capture.isOpened():
             raise Exception("Unable to open video file: {0}".format(video_path))
@@ -316,15 +345,16 @@ def align_demo(path_to_dlc, path_to_file, filename, file_format, crop_size, use_
         frame_count = len(data)  # Change this to an arbitrary number if you first want to test the code
 
     a, n, time_series = align_mouse(path_to_file, filename, file_format, crop_size, pose_list, pose_ref_index,
-                                    pose_flip_ref, bg, frame_count, use_video, interp_flag)
+                                    pose_flip_ref, bg, frame_count, use_video, interp_flag, vid_path=vid_path)
 
     if check_video:
         play_aligned_video(a, n, frame_count)
     # return dividing by 1000 to output in the same scale as input
-    return time_series/1000
+    return time_series/1000, a
 
 
-def run_alignment(path_dlc, path_file, file_format, crop_size, use_video=False, check_video=False):
+def run_alignment(path_dlc, path_file, file_format, crop_size, use_video=False,
+                  check_video=False, save_align=True, video_path=None):
     """Run the egocentric alignment on the target file and save the npy file to the target path"""
 
     # get the file_name
@@ -334,14 +364,16 @@ def run_alignment(path_dlc, path_file, file_format, crop_size, use_video=False, 
     file_name = file_name.replace('_dlc', '')
 
     # call function and save into your VAME data folder
-    egocentric_time_series = align_demo(path_dlc, path_file, file_name, file_format,
-                                        crop_size, use_video=use_video, check_video=check_video)
+    egocentric_time_series, video_frames = align_demo(path_dlc, path_file, file_name, file_format,
+                                                      crop_size, use_video=use_video, check_video=check_video,
+                                                      vid_path=video_path)
 
     # define the output path
     output_path = os.path.join(path_file, 'data', file_name, file_name + '-PE-seq.npy')
-    # np.save(output_path, egocentric_time_series)
+    if save_align:
+        np.save(output_path, egocentric_time_series)
 
-    return egocentric_time_series
+    return egocentric_time_series, video_frames
 
 
 if __name__ == '__main__':
@@ -352,25 +384,31 @@ if __name__ == '__main__':
     #     r"J:\Drago Guggiana Nilo\Prey_capture\AnalyzedData\
     #     11_14_2019_17_24_28_miniscope_dg_190806_a_succ_nofluo_preproc.hdf5"
     # path_dlc = \
-    #     r"J:\Drago Guggiana Nilo\Prey_capture\VideoExperiment\11_11_2019_00_41_27_miniscope_DG_190806_a_fail_nomini_dlc.h5"
+    #     r"J:\Drago Guggiana Nilo\Prey_capture\VideoExperiment\11_14_2019_17_24_28_miniscope_dg_190806_a_succ_nofluo_dlc.h5"
 
     # path_dlc = r"J:\Drago Guggiana Nilo\Prey_capture\AnalyzedData\09_08_2020_15_00_07_miniscope_DG_200701_a_succ_preproc.hdf5"
     # path_dlc = r"J:\Drago Guggiana Nilo\Prey_capture\AnalyzedData\11_11_2019_00_41_27_miniscope_DG_190806_a_fail_nomini_preproc.hdf5"
-    path_dlc = r"J:\Drago Guggiana Nilo\Prey_capture\AnalyzedData\12_16_2019_16_21_34_miniscope_MM_191108_a_fail_preproc.hdf5"
+    # path_dlc = r"J:\Drago Guggiana Nilo\Prey_capture\AnalyzedData\12_16_2019_16_21_34_miniscope_MM_191108_a_fail_preproc.hdf5"
+    path_dlc = r'J:\Drago Guggiana Nilo\Prey_capture\AnalyzedData\09_08_2020_15_26_21_miniscope_DG_200701_a_succ_preproc.hdf5'
     path_vame = r"F:\VAME_projects\VAME_prey-Dec1-2020"
+    # video_path = path_dlc.replace('AnalyzedData', 'VideoExperiment').replace('_preproc.hdf5', '.avi')
+    # video_path = path_dlc.replace('AnalyzedData', 'VideoExperiment').replace('_dlc.h5', '.avi')
+    video_path = r"D:\temp_dlc_process\bounded.avi"
     # fname = r"03_13_2020_13_20_21_miniscope_MM_200129_a_succ"
     file_format = '.avi'
-    crop_size = (1, 1)
-    use_video = False
-    check_video = False
+    crop_size = (200, 200)
+    use_video = True
+    check_video = True
+    save_align = False
 
     egocentric_time_series = run_alignment(path_dlc, path_vame, file_format, crop_size,
-                                           use_video=use_video, check_video=check_video)
+                                           use_video=use_video, check_video=check_video,
+                                           save_align=save_align, video_path=video_path)
 
     # test plot
     import matplotlib.pyplot as plt
     import functions_plotting as fp
-    plt.plot(egocentric_time_series.T)
+    plt.plot(egocentric_time_series[0].T)
     plt.show()
 
     # fp.simple_animation(egocentric_time_series, interval=100)

snakemake_scripts/sub_preprocess_S1.py
@@ -226,9 +226,9 @@ def run_dlc_preprocess(file_path_bonsai, file_path_dlc, save_file, file_info, ke
                                 'corner_BR_x', 'corner_BR_y', 'corner_UR_x', 'corner_UR_y'])
         # get the corners
         corner_points = process_corners(corner_info)
-        if file_info['result'] != 'habi':
-            # interpolate the position of the cricket assuming stationarity
-            filtered_traces = fp.interpolate_animals(filtered_traces, np.nan)
+        # if file_info['result'] != 'habi':
+        #     # interpolate the position of the cricket assuming stationarity
+        #     filtered_traces = fp.interpolate_animals(filtered_traces, np.nan)
         # # eliminate points that separate from either mouse or cricket more than a threshold
         # filtered_traces = \
         #     fp.maintain_animals(filtered_traces, animal_threshold,
@@ -324,6 +324,10 @@ def run_dlc_preprocess(file_path_bonsai, file_path_dlc, save_file, file_info, ke
         # put the values back in the main df
         filtered_traces.loc[:, column_list] = mouse_data
 
+    if file_info['result'] != 'habi':
+        # interpolate the position of the cricket assuming stationarity
+        filtered_traces = fp.interpolate_animals(filtered_traces, np.nan,
+                                                 paths.arena_coordinates[file_info['rig']], corner_points)
     # filtered_traces = \
     #     fp.infer_cricket_position(filtered_traces, cricket_threshold,
     #                               corner_points, paths.arena_coordinates['miniscope'])