fixed visual angle calculation

functions_io.py
@@ -96,8 +96,10 @@ def combine_tif(filenames, processing_path=None):
         im_1 = np.concatenate((im_1, im_n))
         # save the file name and the number of frames
         frames_list.append([filenames[i], im_n.shape[0]])
-    # scale the output to max
-    im_1 = ((im_1/np.max(im_1))*255).astype('uint8')
+    # scale the output to max and turn into uint8 (for MiniAn)
+    max_value = np.max(im_1)
+    for idx, frames in enumerate(im_1):
+        im_1[idx, :, :] = ((frames/max_value)*255).astype('uint8')
     # save the final stack
     io.imsave(out_path_tif, im_1, bigtiff=True)
     # save the info about the files

snakemake_scripts/scatter_calcium.py
@@ -58,7 +58,7 @@ try:
     with h5py.File(calcium_path, 'r') as f:
         # calcium_data = np.array((f['sigfn'])).T
         # calcium_data = np.array(f['estimates/C'])
-        calcium_data = np.array(f['C']).T
+        calcium_data = np.array(f['C'])
         frame_list = np.array(f['frame_list'])
 
     # get the trials in the file

snakemake_scripts/sub_preprocess_S1.py
@@ -139,13 +139,9 @@ def run_preprocess(file_path_bonsai, save_file, file_info,
 
 def run_dlc_preprocess(file_path_bonsai, file_path_dlc, save_file, file_info, kernel_size=5):
     """Extract the relevant columns from the dlc file and rename"""
-    # define the threshold for trimming the trace (in pixels for now)
-    trim_cutoff = 200
+
     # define the likelihood threshold for the DLC points
     likelihood_threshold = 0.8
-    # define the threshold for max amount of cm allowed between points within an animal
-    animal_threshold = 4
-    cricket_threshold = 10
     # just return the output path
     out_path = save_file
     # load the bonsai info
@@ -220,25 +216,6 @@ def run_dlc_preprocess(file_path_bonsai, file_path_dlc, save_file, file_info, ke
                                 'corner_BR_x', 'corner_BR_y', 'corner_UR_x', 'corner_UR_y'])
         # get the corners
         corner_points = fp.process_corners(corner_info)
-        # if file_info['result'] != 'habi':
-        #     # interpolate the position of the cricket assuming stationarity
-        #     filtered_traces = fp.interpolate_animals(filtered_traces, np.nan)
-        # # eliminate points that separate from either mouse or cricket more than a threshold
-        # filtered_traces = \
-        #     fp.maintain_animals(filtered_traces, animal_threshold,
-        #                         corner_points, paths.arena_coordinates['miniscope'])
-        # add the cricket position under the mouse when it is not visible and last position was close to the mouse
-        # filtered_traces = \
-        #     fp.infer_cricket_position(filtered_traces, cricket_threshold,
-        #                               corner_points, paths.arena_coordinates['miniscope'])
-        # set final position of cricket, assume that cricket is under mouse whenever it is not observed
-        # # for succ trials, it stops when mouse stops
-        # if 'succ' in file_path_dlc:
-        #     # check if the last point for the cricket is a NaN
-        #     if np.isnan(filtered_traces['cricket_0_x'].iloc[-1]):
-        #         # if so, make it the position of the mouse head
-        #         filtered_traces[['cricket_0_x', 'cricket_0_y']].iloc[-1] = \
-        #             filtered_traces[['mouse_head_x', 'mouse_head_y']].iloc[-1]
 
     except IndexError:
         # DLC in VR arena
@@ -270,17 +247,6 @@ def run_dlc_preprocess(file_path_bonsai, file_path_dlc, save_file, file_info, ke
         for column in filtered_traces.columns:
             if 'cricket' in column:
                 filtered_traces.drop([column], inplace=True, axis=1)
-
-    # # trim the trace at the last large jump of the mouse trajectory (i.e when the mouse enters the arena)
-    # # do it after median filtering to prevent the single point errors to trim the video too much
-    # # calculate the displacement of the mouse center
-    # mouse_displacement = np.sqrt((np.diff(filtered_traces['mouse_x']))**2 + (np.diff(filtered_traces['mouse_y']))**2)
-    # cutoff_frame = np.argwhere(mouse_displacement > trim_cutoff)
-    # # check if it's empty. if so, don't cutoff anything
-    # if cutoff_frame.shape[0] > 0:
-    #     cutoff_frame = cutoff_frame[-1][0]
-    # else:
-    #     cutoff_frame = 0
 
     # save a copy of the untrimmed traces for later
     untrimmed = filtered_traces.copy()
@@ -326,51 +292,10 @@ def run_dlc_preprocess(file_path_bonsai, file_path_dlc, save_file, file_info, ke
         # interpolate the position of the cricket assuming stationarity
         filtered_traces = fp.interpolate_animals(filtered_traces, np.nan,
                                                  paths.arena_coordinates[file_info['rig']], corner_points, untrimmed)
-    # filtered_traces = \
-    #     fp.infer_cricket_position(filtered_traces, cricket_threshold,
-    #                               corner_points, paths.arena_coordinates['miniscope'])
-    # # interpolate the position of the cricket assuming stationarity
-    # filtered_traces = fp.interpolate_animals(filtered_traces, np.nan)
-
-    # # get only the cricket data
-    # column_list = [el for el in filtered_traces.columns if ('cricket' in el) and ('_x' in el)]
-    # column_list += [el for el in filtered_traces.columns if ('cricket' in el) and ('_y' in el)]
-    # cricket_data = filtered_traces.loc[:, column_list].to_numpy()
-    #
-    # # repair the trajectory
-    # cricket_data = pose_repair(cricket_data, os.path.join(paths.pose_repair_path, 'cricket'), delay=2)
-    #
-    # # put the values back in the main df
-    # filtered_traces.loc[:, column_list] = cricket_data
-
-    # # use an ARIMA to infer the low likelihood points
-    # filtered_traces = fp.median_arima(filtered_traces, filtered_traces.columns)
-    # interpolate the NaN stretches
-    # filtered_traces = fp.interpolate_segments(filtered_traces, np.nan)
-    # filtered_traces = fp.interpolate_animals(filtered_traces, np.nan)
 
     # median filter the traces
     filtered_traces = fp.median_discontinuities(filtered_traces, filtered_traces.columns, kernel_size)
 
-    # trim the trace before and after the places with speed above threshold
-    # calculate speed trace for the mouse
-
-    # filtered_traces
-
-    # find the places where there is no pixel movement in any axis and NaN those
-
-    # cricket_nonans_x = filtered_traces['cricket_x']
-    # cricket_nonans_y = filtered_traces['cricket_y']
-    # find the places with tracking off via delta pixel and NaN them
-    # filtered_traces = find_frozen_tracking(filtered_traces, stretch_length=15)
-    # eliminate discontinuities before interpolating
-    # filtered_traces = nan_large_jumps(filtered_traces, ['cricket_x', 'cricket_y'], max_step=200, max_length=150)
-    # eliminate large jumps
-    # filtered_traces = nan_jumps_dlc(filtered_traces)
-    # # eliminate isolated points
-    # filtered_traces = eliminate_singles(filtered_traces)
-    # interpolate the NaN stretches
-    # filtered_traces = interpolate_segments(filtered_traces, np.nan)
     # parse the bonsai file for the time stamps
     timestamp = []
     bonsai_data = []

snakemake_scripts/sub_preprocess_S2.py
@@ -1,6 +1,8 @@
 # imports
 from functions_kinematic import *
+import functions_preprocessing as fp
 import pandas as pd
+import scipy as sc
 
 
 def cricket_processing(cricket_coord, data, mouse_coord, mouse_heading, kine_data, vr=False, cricket_name='cricket_0'):
@@ -45,17 +47,89 @@ def cricket_processing(cricket_coord, data, mouse_coord, mouse_heading, kine_dat
     # if the head data from DLC is available and there's no motive, calculate head direction and delta_look
     if 'mouse_head_x' in data.columns and 'mouse_x_m' not in data.columns and vr is False:
 
+        # define the factor for cricket size (see onenote)
+        cricket_proportion = 1.73
+        cricket_to_head = 0.19
+        cricket_to_tail = 0.57
+        # define the width to length relationship to find the bounding box coordinates
+        cricket_aspect_ratio = 0.288
+        # calculate cricket size
+        c_size = fp.cricket_size(data, cricket_proportion)
+        # save in the dataframe
+        cricket_data[cricket_name+'_size'] = c_size
+
+        # get the mouse coordinates
+        mouse_coord_numpy = data[['mouse_head_x', 'mouse_head_y']].to_numpy()
+
         # also get the cricket to head angle
         head_cricket = heading_calculation(data[['cricket_0_x', 'cricket_0_y']].to_numpy(),
-                                           data[['mouse_head_x', 'mouse_head_y']].to_numpy())
+                                           mouse_coord_numpy)
         # finally, get the delta_head angle
         delta_head = head_cricket - kine_data.head_direction
         # effectively wrap around -180 and 180 to put the center at 0
         delta_head[delta_head > 180] += -180
         delta_head[delta_head < -180] += 180
+        # change nans to 0 as these happen when cricket and mouse overlap
+        delta_head[np.isnan(delta_head)] = 0
         # save in the dataframe
         cricket_data[cricket_name+'_delta_head'] = delta_head
 
+        # generate a vector with the FOV quadrant the cricket is in
+        # 0 is binocular, 1 is left eye, 2 is right eye, 3 is out of view, assume 280 deg total FOV per eye
+        # and 40 deg overlap (from Meyer et al. 2018)
+        fov_quadrant = np.zeros_like(delta_head)
+        # create a vector with the delta head data
+        delta_head_array = delta_head.to_numpy()
+        fov_quadrant[(-140 < delta_head_array) & (delta_head_array < -20)] = 1
+        fov_quadrant[(140 > delta_head_array) & (delta_head_array > 20)] = 2
+        fov_quadrant[np.abs(delta_head_array) >= 140] = 3
+        # save the vector in the dataframe
+        cricket_data[cricket_name+'_quadrant'] = fov_quadrant
+
+        # calculate the visual angle subtended by the cricket
+        # get the unit vector for the cricket body
+        delta_vector = data[['cricket_0_head_x', 'cricket_0_head_y']].to_numpy() \
+            - data[['cricket_0_x', 'cricket_0_y']].to_numpy()
+        # get the length of the head point to body point vector
+        distances = np.linalg.norm(delta_vector, axis=1)
+        unit_vector = delta_vector/np.expand_dims(np.linalg.norm(delta_vector, axis=1), axis=1)
+        # get the x and y coordinates of the edges of the cricket
+        head_point = data[['cricket_0_head_x', 'cricket_0_head_y']].to_numpy() + \
+            np.expand_dims(distances, axis=1)*cricket_to_head*unit_vector
+        tail_point = data[['cricket_0_x', 'cricket_0_y']].to_numpy() - \
+            np.expand_dims(distances, axis=1)*cricket_to_tail*unit_vector
+        # get the perpendicular to the unit vector
+        unit_ortho = np.array([[np.cos(np.pi/2), -np.sin(np.pi/2)], [np.sin(np.pi/2), np.cos(np.pi/2)]])@unit_vector.T
+        unit_ortho = unit_ortho.T
+        # now get the corners of the bounding box
+        head_corner_1 = head_point + \
+            np.expand_dims(distances, axis=1)*cricket_proportion*cricket_aspect_ratio*unit_ortho
+        head_corner_2 = head_point - \
+            np.expand_dims(distances, axis=1)*cricket_proportion*cricket_aspect_ratio*unit_ortho
+        tail_corner_1 = tail_point + \
+            np.expand_dims(distances, axis=1)*cricket_proportion*cricket_aspect_ratio*unit_ortho
+        tail_corner_2 = tail_point - \
+            np.expand_dims(distances, axis=1)*cricket_proportion*cricket_aspect_ratio*unit_ortho
+        # put them on a list for for loops
+        corner_list = [head_corner_1, head_corner_2, tail_corner_1, tail_corner_2]
+        # get the angles
+        angle_list = [heading_calculation(el, mouse_coord_numpy) + 360
+                      for el in corner_list]
+        # turn into an array
+        angle_list = np.array(angle_list).T
+        # get the angles
+        visual_angle = np.max(np.array([sc.spatial.distance.pdist(np.expand_dims(el, axis=1)) for el in angle_list]),
+                              axis=1)
+        # visual_angle = np.apply_along_axis(sc.spatial.distance.pdist, 1, np.expand_dims(angle_list.T, axis=2))
+        # # get the angles of these coordinates, including an offset to wrap
+        # head_angle = heading_calculation(head_vector,
+        #                                  data[['mouse_head_x', 'mouse_head_y']].to_numpy()) + 360
+        # tail_angle = heading_calculation(tail_vector,
+        #                                  data[['mouse_head_x', 'mouse_head_y']].to_numpy()) + 360
+        # # get the delta angle, i.e. the visual angle
+        # visual_angle = head_angle - tail_angle
+        # save the coordinates in the dataframe
+        cricket_data[cricket_name+'_visualangle'] = visual_angle
     return cricket_data
 
 
@@ -125,7 +199,6 @@ def kinematic_calculations(name, data):
         # get the head angle with respect to body
         head_direction = heading_calculation(data[['mouse_head_x', 'mouse_head_y']].to_numpy(),
                                              data[['mouse_x', 'mouse_y']].to_numpy())
-
         # save in the dataframe
         kine_data['head_direction'] = head_direction
 