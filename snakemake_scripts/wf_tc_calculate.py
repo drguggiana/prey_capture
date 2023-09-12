0562b0188688d4fb744333e6539a3bea50203c43
matt m <matthew.mccann13@gmail.com>
2023-09-12 13:19:04 +0200

    tuning curve day consolidation

--- snakemake_scripts/wf_tc_calculate.py
+++ snakemake_scripts/wf_tc_calculate.py
@@ -147,12 +147,12 @@ def parse_kinematic_data(matched_calcium):
 if __name__ == '__main__':
     # get the data paths
     try:
-        input_path = list(snakemake.input)
+        input_path = list(snakemake.input[0])
         # get the slugs
         slug_list = [os.path.basename(el).replace('_preproc.hdf5', '') for el in input_path]
         # read the output path and the input file urls
         out_path = snakemake.output[0]
-        data_all = snakemake.params.file_info
+        data_all = snakemake.params.info
         # get the parts for the file naming
         name_parts = os.path.basename(out_path).split('_')
         day = '_'.join(name_parts[:3])
