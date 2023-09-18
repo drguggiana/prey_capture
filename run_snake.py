75b75a818172395374e929c4f12988f7e7be5fc6
matt m <matthew.mccann13@gmail.com>
2023-09-18 16:21:02 +0200

    Latency fix

--- run_snake.py
+++ run_snake.py
@@ -110,14 +110,15 @@ for idx, target_entries in enumerate(full_queries):
     preprocess_sp = sp.Popen(['snakemake', out_path, out_path, '--cores', '1',
                               '-s', paths.snakemake_scripts,
                               '-d', paths.snakemake_working,
+                              '--latency-wait', '60',
                               # '--use-conda',
                               # '-F',         # (hard) force rerun everything
-                              '-f',         # (soft) force rerun last step
+                              # '-f',         # (soft) force rerun last step
                               # '--unlock',   # unlocks the files after force quit
                               '--rerun-incomplete',
                               '--verbose',  # make the output more verbose for debugging
                               # '--debug-dag',  # show the file selection operation, also for debugging
-                              '--dryrun',  # generates the DAG and everything, but doesn't process,
+                              # '--dryrun',  # generates the DAG and everything, but doesn't process,
                               # '--reason',  # print the reason for executing each job
                               ],
                              stdout=sp.PIPE,
