Corrected circular variance metric

functions_tuning.py
@@ -247,7 +247,7 @@ def calculate_pref_von_mises(angles, tuning_curve, fit_kind, **kwargs):
 
     init_params = [amp, np.deg2rad(mean), kappa, amp, np.deg2rad(mean2), kappa]
     lower_bound = [0, 0, 1, 0, 0, 1]
-    upper_bound = [2, 2*np.pi, 15, 2, 2*np.pi, 15]
+    upper_bound = [1, 2*np.pi, 12, 1, 2*np.pi, 12]
 
     # Run regression
     fit = least_squares(fit_double_von_mises_pdf, init_params,
@@ -375,7 +375,7 @@ def generate_response_vector(responses, function, **kwargs):
     return resp, angles
 
 
-def bootstrap_responsivity(angles, magnitudes, num_shuffles=1000):
+def bootstrap_responsivity(angles, magnitudes, multiplier, num_shuffles=1000):
     
     shuffled_responsivity = []
     shuffled_circ_var = []
@@ -390,8 +390,10 @@ def bootstrap_responsivity(angles, magnitudes, num_shuffles=1000):
         # shuffled_response_vector, angles = generate_response_vector(shuffled_responses, np.nanmean, tuning_kind)
         
          #-- Get response circular variance --#
-        circ_var = circ.var(angles, w=magnitudes, d=angle_sep)
-        responsivity = 1 - circ_var
+        resultant_length = circ.resultant_vector_length(angles, w=magnitudes, d=angle_sep, axial_correction=multiplier)
+        circ_var = 1 - resultant_length
+        responsivity = resultant_length
+
         shuffled_circ_var.append(circ_var)
         shuffled_responsivity.append(responsivity)
         

snakemake_scripts/wf_tc_calculate.py
@@ -64,16 +64,22 @@ def calculate_visual_tuning(activity_df, tuning_kind, tuning_fit='von_mises', bo
                                          type=processing_parameters.gof_type)
 
         # -- Get resultant vector and response variance-- #
+        if 'direction' in tuning_kind:
+            multiplier = 1.
+        else:
+            multiplier = 2.
+
         thetas = np.deg2rad(norm_trial_activity[tuning_kind].to_numpy())
         theta_sep = np.mean(np.diff(thetas))
         magnitudes = norm_trial_activity[cell].to_numpy()
 
-        resultant_length = circ.resultant_vector_length(thetas, w=magnitudes, d=theta_sep)
-        resultant_angle = circ.mean(thetas, w=magnitudes, d=theta_sep)
+        resultant_length = circ.resultant_vector_length(thetas, w=magnitudes, d=theta_sep, axial_correction=multiplier)
+        resultant_angle = circ.mean(thetas, w=magnitudes, d=theta_sep, axial_correction=multiplier)
         resultant_angle = np.rad2deg(resultant_angle)
 
-        circ_var = circ.var(thetas, w=magnitudes, d=theta_sep)
-        responsivity = 1 - circ_var
+        circ_var = 1 - resultant_length
+        responsivity = resultant_length
+
 
         # -- Run permutation tests -- #
         # Calculate fit on subset of data
@@ -84,7 +90,8 @@ def calculate_visual_tuning(activity_df, tuning_kind, tuning_fit='von_mises', bo
         p_gof = percentileofscore(bootstrap_gof[~np.isnan(bootstrap_gof)], fit_gof, kind='mean') / 100.
 
         # Shuffle the trial IDs and compare the real selectivity index to the bootstrapped distribution
-        bootstrap_responsivity = tuning.bootstrap_responsivity(thetas, magnitudes, num_shuffles=bootstrap_shuffles)
+        bootstrap_responsivity = tuning.bootstrap_responsivity(thetas, magnitudes, multiplier,
+                                                               num_shuffles=bootstrap_shuffles)
         p_res = percentileofscore(bootstrap_responsivity, responsivity, kind='mean') / 100.
 
         cell_data = [mean_trial_activity[[tuning_kind, cell]].to_numpy(),
@@ -96,7 +103,7 @@ def calculate_visual_tuning(activity_df, tuning_kind, tuning_fit='von_mises', bo
                      fit, fit_curve, fit_gof, bootstrap_gof, p_gof,
                      pref_angle, bootstrap_pref_angle, real_pref_angle, bootstrap_real_pref,
                      (resultant_length, resultant_angle),
-                     circ_var, responsivity, bootstrap_responsivity, p_res]
+                     circ_var, responsivity, bootstrap_responsivity, p_res, is_resp]
 
         cell_data_list.append(cell_data)
 
@@ -106,7 +113,7 @@ def calculate_visual_tuning(activity_df, tuning_kind, tuning_fit='von_mises', bo
                  'fit', 'fit_curve', 'gof', 'bootstrap_gof', 'p_gof',
                  'pref', 'bootstrap_pref', 'shown_pref', 'bootstrap_shown_pref',
                  'resultant',
-                 'circ_var', 'responsivity', 'bootstrap_responsivity', 'p_responsivity']
+                 'circ_var', 'responsivity', 'bootstrap_responsivity', 'p_responsivity', 'Resp_test']
 
     data_df = pd.DataFrame(index=cells, columns=data_cols, data=cell_data_list)
     return data_df

snakemake_scripts/wf_tc_consolidate.py
@@ -1,15 +1,37 @@
 import os
 import numpy as np
 import pandas as pd
+import warnings
 
 import paths
 import processing_parameters
 import functions_bondjango as bd
+import functions_data_handling as dh
 import functions_misc as fm
+from functions_kinematic import wrap_negative
 
-def fraction_responsive(ds):
+warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
+
+
+def kine_fraction_responsive(ds):
     return ds['Resp_test'].sum() / ds['Resp_test'].count()
 
+
+def vis_frac_responsive(ds):
+    is_resp = ds['responsivity'] > 0.25
+    return is_resp.sum() / is_resp.count()
+
+
+def dicts_to_dataframe(dicts, index):
+    df_list = []
+    for d, idx in zip(dicts, index):
+        vals = np.array(list(d.values()))
+        cols = np.array(list(d.keys()))
+        df = pd.DataFrame(data=np.expand_dims(vals, axis=-1).T, columns=cols, index=[idx])
+        df_list.append(df)
+
+    df_concat = pd.concat(df_list)
+    return df_concat
 
 
 if __name__ == '__main__':
@@ -26,11 +48,15 @@ if __name__ == '__main__':
     except NameError:
         # get the search string
         search_string = processing_parameters.search_string
+        parsed_search = dh.parse_search_string(search_string)
 
         # get the paths from the database
         all_path = bd.query_database('analyzed_data', search_string)
-        input_paths = [el['analysis_path'] for el in all_path if '_tcday' in el['slug']]
-        rigs = [el['rig'] for el in all_path if '_tcday' in el['slug']]
+        input_paths = np.array([el['analysis_path'] for el in all_path if ('_tcday' in el['slug']) and
+                                (parsed_search['mouse'].lower() in el['slug'])])
+        day = np.unique([path[:4] for path in input_paths])
+        slug = [os.path.basename(el) for el in input_paths]
+        rigs = np.array([el.split('_')[6] for el in slug])
 
         # assemble the output path
         out_path = os.path.join(paths.analysis_path, 'test_tcconsolidate.hdf5')
@@ -40,10 +66,6 @@ if __name__ == '__main__':
     matches = None
     empty_flag = False
     data_list = []
-
-    rev_sort = np.argsort(rigs)[::-1]
-    rigs = rigs[rev_sort]
-    input_paths = input_paths[rev_sort]
 
     for file in input_paths:
         # Load the data
@@ -70,27 +92,82 @@ if __name__ == '__main__':
         if matches is not None:
             matches.to_hdf(out_path, 'cell_matches')
 
-        summary_stats = {}
+        visual_shifts = {}
         for data, rig in zip(data_list, rigs):
+            all_cells_stats = {}
+            matched_stats = {}
+            unmatched_stats = {}
+
             # get matches and save
             match_col = np.where([rig in el for el in matches.columns])[0][0]
             match_idxs = matches.iloc[:, match_col].to_numpy(dtype=int)
+            num_cells = len(data['norm_spikes_viewed_direction_props'].index)
 
-            for feature in data.keys():
+            all_cells_stats['num_cells'] = num_cells
+            all_cells_stats['num_matches'] = len(match_idxs)
+            all_cells_stats['match_frac'] = len(match_idxs)/num_cells
+            matched_stats['num_cells'] = len(match_idxs)
+            unmatched_stats['num_cells'] = num_cells - len(match_idxs)
+
+            kine_features = [el for el in data.keys() if not any([x in el for x in ['props', 'counts', 'edges']])]
+            vis_features = [el for el in data.keys() if 'props' in el]
+
+            for feature in kine_features:
                 # Save the whole dataset
                 data[feature].to_hdf(out_path, f'{rig}/all_cells/{feature}')
+                all_cells_stats[f"frac_resp_{feature}"] = kine_fraction_responsive(data[feature])
 
                 # Save matched TCs
-                if ('counts' not in feature) and ('edges' not in feature):
-                    matched_feature = data[feature].iloc[match_idxs, :].reset_index(names=['original_cell_id'])
-                    matched_feature.to_hdf(out_path, f'{rig}/matched/{feature}')
+                matched_feature = data[feature].iloc[match_idxs, :].reset_index(names=['original_cell_id'])
+                matched_feature.to_hdf(out_path, f'{rig}/matched/{feature}')
+                matched_stats[f"frac_resp_{feature}"] = kine_fraction_responsive(matched_feature)
 
-                    # save unmatched tcs
-                    unmatched_feature = data[feature].df.drop(match_idxs, axis=0).reset_index(names=['original_cell_id'])
-                    unmatched_feature.to_hdf(out_path, f'{rig}/unmatched/{feature}')
+                # save unmatched tcs
+                unmatched_feature = data[feature].drop(match_idxs, axis=0)
+                unmatched_feature.to_hdf(out_path, f'{rig}/unmatched/{feature}')
+                unmatched_stats[f"frac_resp_{feature}"] = kine_fraction_responsive(unmatched_feature)
 
+            exp_vis_features = {}
+            for feature in vis_features:
+                feat = '_'.join(feature.split('_')[3:-1])
+                # Save the whole dataset
+                data[feature].reset_index(names=['original_cell_id']).to_hdf(out_path, f'{rig}/all_cells/{feature}')
+                all_cells_stats[f"frac_resp_{feat}"] = vis_frac_responsive(data[feature])
 
-    # generate database entry
+                # Save matched TCs
+                matched_feature = data[feature].iloc[match_idxs, :].reset_index(names=['original_cell_id'])
+                matched_feature.to_hdf(out_path, f'{rig}/matched/{feature}')
+                matched_stats[f"frac_resp_{feat}"] = vis_frac_responsive(matched_feature)
+
+                # Get the preferred orientation/direction from the matched cells
+                exp_vis_features[feat] = matched_feature.loc[:, ['pref', 'responsivity', 'p_responsivity', 'bootstrap_responsivity']]
+
+                # save unmatched tcs
+                unmatched_feature = data[feature].reset_index(names=['original_cell_id']).drop(index=match_idxs)
+                unmatched_feature.to_hdf(out_path, f'{rig}/unmatched/{feature}')
+                unmatched_stats[f"frac_resp_{feat}"] = vis_frac_responsive(unmatched_feature)
+
+            summary_stats = dicts_to_dataframe([all_cells_stats, matched_stats, unmatched_stats],
+                                               ['all_cells', 'matched', 'unmatched'])
+            summary_stats.to_hdf(out_path, f'{rig}/summary_stats')
+
+            visual_shifts[rig] = exp_vis_features
+
+        # Calculate delta prefs for all matched cells
+        # use the fixed rig as the reference
+        delta_pref = pd.DataFrame()
+        for tuning_type in visual_shifts[rigs[0]].keys():
+            fixed = visual_shifts[rigs[0]][tuning_type]
+            free = visual_shifts[rigs[1]][tuning_type]
+            diff = fixed.loc[:, ['pref']].subtract(free.loc[:, ['pref']]).to_numpy()
+            if 'direction' in tuning_type:
+                diff = wrap_negative(diff.flatten())
+            delta_pref[f"delta_{tuning_type}"] = diff
+
+        delta_pref.to_hdf(out_path, 'delta_vis_tuning')
+
+        print('hi')
+
     # assemble the entry data
     entry_data = {
         'analysis_type': 'tc_consolidate',
@@ -100,7 +177,7 @@ if __name__ == '__main__':
         'result': 'multi',
         'rig': 'multi',
         'lighting': 'multi',
-        'imaging': 'multi',
+        'imaging': 'wirefree',
         'slug': fm.slugify(os.path.basename(out_path)[:-5]),
 
     }