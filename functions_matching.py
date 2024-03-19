Fixed calculation of running wheel speed

functions_matching.py
@@ -1111,23 +1111,29 @@ def match_wheel(file_info, filtered_traces, wheel_diameter=16):
     else:
         sync_data.columns = ['Time', 'projector_frames', 'camera_frames',
                              'sync_trigger', 'mini_frames', 'wheel_frames', 'projector_frames_2']
-    #
-    # sync_data = pd.read_csv(file_info['sync_path'], names=['Time', 'projector_frames', 'camera_frames',
-    #                                                        'sync_trigger', 'mini_frames', 'wheel_frames'],
-    #                         index_col=False)
+
     # get the wheel trace
     wheel_position = sync_data.loc[filtered_traces['sync_frames'], ['wheel_frames']]
-    # convert the position to radians
+
+    # convert the position (fraction of rotation around the wheel) to radians
     wheel_position = (wheel_position - wheel_position.min()) / (wheel_position.max() - wheel_position.min()) * 2 * np.pi
+
     # unwrap
     wheel_position = np.unwrap(wheel_position).flatten()
-    # get the speed of the wheel
-    wheel_speed = np.diff(wheel_position * np.pi * (wheel_diameter - 1) / 360)
-    # get the wheel acceleration
+
+    # get instantaneous arc length (angular position * radius) in cm
+    arc_length = wheel_position * (wheel_diameter - 1) / 2
+
+    # get the instantaneous speed of the wheel in cm/s
+    wheel_speed = np.diff(arc_length)
+
+    # get the wheel acceleration in cm/s^2
     wheel_acceleration = np.diff(wheel_speed)
+
     # prepend zeros to speed and acceleration arrays
     wheel_speed = np.insert(wheel_speed, 0, 0, axis=0)
     wheel_acceleration = np.insert(wheel_acceleration, [0, 0], 0, axis=0)
+    
     # save in the output frame
     filtered_traces['wheel_speed'] = wheel_speed
     filtered_traces['wheel_acceleration'] = wheel_acceleration

functions_plotting.py
@@ -1,14 +1,17 @@
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-from matplotlib import animation, ticker
 import numpy as np
-import sklearn.cluster as clu
+import matplotlib.pyplot as plt
+import seaborn as sns
 import holoviews as hv
+import sklearn.cluster as clu
+from mpl_toolkits.mplot3d import Axes3D
+from functools import partial
+from matplotlib import animation, ticker
 from bokeh.themes.theme import Theme
 from bokeh.plotting import show as bokeh_show
-from functools import partial
-from processing_parameters import gof_type
+
+import processing_parameters
 from functions_kinematic import wrap
+
 
 # define in to cm
 constant_in2cm = 2.54
@@ -600,6 +603,57 @@ def histogram(data_in, rows=1, columns=1, bins=50, fig=None, color=None, fontsiz
             # update the plot counter
             plot_counter += 1
     return fig
+
+
+def violin_swarm(ds, save_path, backend='hvplot', save=False, cmap='blue', 
+                 xlabel='', ylabel='',
+                 width=1500, height=1000, font_size='screen', dpi=800):
+
+    rename_dict = dict(zip(list(ds.columns), [processing_parameters.wf_label_dictionary_wo_units[col] for col in list(ds.columns)]))
+
+    ds = ds.rename(columns=rename_dict)
+
+    if backend=='hvplot':
+        violinplot = ds[list(rename_dict.values())].hvplot.violin(legend=False, inner='quartiles', color=cmap)
+        violinplot.opts(xlabel=xlabel, ylabel=ylabel, ylim=(-0.05, 1.05), xrotation=45, width=width, height=height)
+        if save:
+            violinplot = save_figure(violinplot, save_path=save_path, fig_width=width, dpi=dpi, fontsize='screen', target='both', display_factor=0.1)
+        else:
+            violinplot = save_figure(violinplot, save_path=save_path, fig_width=width, dpi=dpi, fontsize='screen', target='screen', display_factor=0.1)
+        return violinplot
+    
+    elif backend=='seaborn':
+        swarm_palette = {k:'k' for k in rename_dict.values()}
+        fig, ax = plt.subplots(figsize=(width, height))
+        violinplot = sns.violinplot(data=ds[list(rename_dict.values())], color=cmap, native_scale=True, width=1)
+        violinplot = sns.stripplot(data=ds[list(rename_dict.values())], size=2, palette=swarm_palette, marker="x", linewidth=1)
+        ax.set_ylim((-0.05, 1.05))
+        violinplot.spines[['right', 'top']].set_visible(False)
+        font_size = int(font_sizes_raw[font_size]['xlabel'][:-2])
+        violinplot.set_xlabel(xlabel, fontsize=font_size)
+        violinplot.set_ylabel(ylabel, fontsize=font_size)
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+
+        if save:
+            plt.savefig(save_path, dpi=dpi, format='png')
+
+        return violinplot
+    else:
+        return Exception('Invalid backend')
+    
+
+def hv_hist(ds, key, label, drop_na=True, xlabel=''):
+    data = ds[key].copy()
+
+    if drop_na:
+        data.replace([np.inf, -np.inf], np.nan, inplace=True)
+        data.dropna(inplace=True)
+        data = data[data >= 0]
+        
+    frequencies, edges = np.histogram(data, 20)
+    hist = hv.Histogram((edges, frequencies), label=label).opts(xlabel=xlabel, ylabel='Freq.')
+    return hist
 
 
 def plot_arrow(trajectory, centers, heading, head, cricket, angles, angles2, fig=None):