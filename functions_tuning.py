Fixed DSI/OSI calculation

functions_tuning.py
@@ -4,6 +4,8 @@ import pycircstat as circ
 from scipy.special import i0
 from scipy.optimize import least_squares, curve_fit
 from sklearn.metrics import r2_score, mean_squared_error
+
+import processing_parameters
 import functions_kinematic as fk
 from functions_misc import find_nearest
 
@@ -282,6 +284,7 @@ def generate_response_vector(responses, function, **kwargs):
     resp = responses.groupby([tuning_kind])[cell_ids].agg(function, **kwargs).fillna(0).reset_index()
     angles = resp[tuning_kind].to_numpy()
     resp = resp[cell_ids]
+    resp.insert(0, tuning_kind, angles)
 
     return resp, angles
 
@@ -390,6 +393,11 @@ def calculate_dsi_osi_resultant(angles, magnitudes, bootstrap=False):
     # DSI calculation from https://doi.org/10.1038/nature19818
     # OSI calculation from https://doi.org/10.1523/JNEUROSCI.0095-13.2013
 
+    # First check if the magnitudes are all > 0
+    if np.all(magnitudes == 0):
+        return np.NaN, np.NaN, np.NaN, np.NaN, np.NaN, np.NaN
+
+    # IF we're bootstrapping, we need to get the unique angles and their mean magnitudes
     if bootstrap:
         unique_angles = np.unique(angles)
         tc = [np.nanmean(magnitudes[angles == angle]) for angle in unique_angles]
@@ -400,16 +408,17 @@ def calculate_dsi_osi_resultant(angles, magnitudes, bootstrap=False):
     half_period_angles_1 = angles[angles <= np.pi]
     half_period_mags_1 = magnitudes[angles <= np.pi]
 
-    res_mag_1, res_angle_1 = resultant_vector(half_period_angles_1, half_period_mags_1, 2)
+    res_mag_1, res_angle_1 = resultant_vector(half_period_angles_1, half_period_mags_1, 1)
     res_angle_1 = fk.wrap(res_angle_1, bound=np.pi)     # Need this correction because pycircstat does mod2pi by default
     closest_idx1 = np.argmin(np.abs(angles - res_angle_1))
     resp1 = magnitudes[closest_idx1]
+    angle1 = angles[closest_idx1]
 
     # Get resultant on second half of the data
-    half_period_angles_2 = fk.wrap(angles[angles > np.pi], bound=np.pi)
+    half_period_angles_2 = fk.wrap(angles[angles > np.pi], bound=np.deg2rad(180.1))
     half_period_mags_2 = magnitudes[angles > np.pi]
 
-    res_mag_2, res_angle_2 = resultant_vector(half_period_angles_2, half_period_mags_2, 2)
+    res_mag_2, res_angle_2 = resultant_vector(half_period_angles_2, half_period_mags_2, 1)
     res_angle_2 = fk.wrap(res_angle_2, bound=np.pi)
     res_angle_2 += np.pi
     closest_idx2 = np.argmin(np.abs(angles - res_angle_2))
@@ -428,9 +437,17 @@ def calculate_dsi_osi_resultant(angles, magnitudes, bootstrap=False):
 
     # for dsi
     closest_idx_to_null = np.argmin(np.abs(angles - fk.wrap(pref + np.pi, bound=2*np.pi)))
-    resp_null = magnitudes[closest_idx_to_null]
-    dsi_nasal_temporal = (resp_pref - resp_null) / (resp_pref + resp_null)
-    dsi_abs = 1 - (resp_null/resp_pref)
+    resp_null = magnitudes[closest_idx_to_null-1:closest_idx_to_null+2].mean()
+
+    if resp_pref + resp_null == 0:
+        dsi_nasal_temporal = 0
+    else:
+        dsi_nasal_temporal = (resp_pref - resp_null) / (resp_pref + resp_null)
+
+    if resp_pref == 0:
+        dsi_abs = 0
+    else:
+        dsi_abs = 1 - (resp_null/resp_pref)
 
     # for osi
     closest_idx_to_null_1 = np.argmin(np.abs(angles - fk.wrap(pref + np.pi/2, bound=2*np.pi)))
@@ -438,7 +455,11 @@ def calculate_dsi_osi_resultant(angles, magnitudes, bootstrap=False):
     resp_null_1 = magnitudes[closest_idx_to_null_1]
     resp_null_2 = magnitudes[closest_idx_to_null_2]
     resp_null_osi = np.nanmean([resp_null_1, resp_null_2])
-    osi = (resp_pref - resp_null_osi) / (resp_pref + resp_null_osi)
+
+    if resp_pref + resp_null_osi == 0:
+        osi = 0
+    else:
+        osi = (resp_pref - resp_null_osi) / (resp_pref + resp_null_osi)
     
     return dsi_nasal_temporal, dsi_abs, osi, resultant_length, pref, null
 
@@ -595,22 +616,59 @@ def calculate_dff(ds, baseline_type='iti_mean', **kwargs):
     cells = [el for el in ds.columns if "cell" in el]
     ds = ds.replace([np.inf, -np.inf], 0).fillna(0)
     dff = ds.copy()
-    mask = dff[cells] < 0
-    dff.loc[:, cells].mask(mask, 0.0, inplace=True)
+    # mask = dff[cells] < 0
+    # ds.loc[:, cells].mask(mask, 0.0, inplace=True)
 
     if baseline_type == 'iti_mean':
-        baselines = ds[ds.direction == -1000][cells].mean(axis=0).copy()
+        baselines = ds[ds.trial_num == 0][cells].mean(axis=0).copy()
 
     elif baseline_type == 'quantile':
-        baselines = ds[cells].quantile(kwargs.get('quantile', 0.20), numeric_only=True, axis=0).copy()
+        qaunt = kwargs.get('quantile', 0.20)
+        baselines = ds[cells].quantile(qaunt, numeric_only=True, axis=0).copy()
 
     else:
         raise NameError('Invalid baseline type')
     
-    # Where the quantile is zero, replace with the mean baseline of all nonzeros cells
-    baselines[baselines == 0] = baselines[baselines != 0].mean()
+    # Where the quantile is zero, replace with the mean baseline of all nonzero cells
+    baselines[baselines == 0] = baselines[baselines > 0].mean()
     
     dff[cells] -= baselines
     dff[cells] /= baselines
 
     return dff
+
+
+def parse_trial_frames(df, pre_trial=0, post_trial=0):
+    trial_idx_frames = df[df.trial_num >= 1.0].groupby(['trial_num']).apply(
+        lambda x: [x.index[0] - int(pre_trial * processing_parameters.wf_frame_rate),
+                   x.index[0], x.index[-1],
+                   x.index[-1] + int(post_trial * processing_parameters.wf_frame_rate) + 1]
+                ).to_numpy()
+    trial_idx_frames = np.vstack(trial_idx_frames)
+
+    if trial_idx_frames[0, 0] < df.index[0]:
+        trial_idx_frames[0, 0] = df.index[0]
+
+    if trial_idx_frames[-1, -1] > df.index[-1]:
+        trial_idx_frames[-1, -1] = df.index[-1]
+
+    if trial_idx_frames[-1, -2] == trial_idx_frames[-1, -1]:
+        trial_idx_frames[-1, -2:] = int(df.index[-1]) - 1
+
+    # Get the shifts from the zero point (important for plotting)
+    max_zero_idx_shift = np.max(trial_idx_frames[:, 1] - trial_idx_frames[:, 0])
+
+    traces = []
+    for i, frame in enumerate(trial_idx_frames):
+        df_slice = df.iloc[frame[0]:frame[-1], :].copy()
+        df_slice['frame_num'] = df_slice.loc[frame[1], 'trial_num']
+        # df_slice['direction'] = df_slice.loc[frame[1] + 1, 'direction']
+        # df_slice['direction_wrapped'] = df_slice.loc[frame[1] + 1, 'direction_wrapped']
+        # df_slice['orientation'] = df_slice.loc[frame[1] + 1, 'orientation']
+        # zero_idx_shift = np.abs((frame[1] - frame[0]) - max_zero_idx_shift)
+        # df_slice['zero_idx_shift'] = zero_idx_shift
+
+        traces.append(df_slice)
+
+    traces = pd.concat(traces, axis=0).reset_index(drop=True)
+    return traces, trial_idx_frames

run_wf_main_analysis.py
@@ -519,8 +519,7 @@ def create_umap_plot(plot_data: np.ndarray, predictor_column: str) -> hv.Scatter
                    yaxis=None, tools=['hover'])
 
     if any([predictor_column.startswith('dsi'), predictor_column.startswith('osi')]):
-        umap_plot.opts(title=f"{predictor_column[:3].upper()} "
-                             f"{processing_parameters.wf_label_dictionary_wo_units[predictor_column.split('_')[-1]]}")
+        umap_plot.opts(title=f"{predictor_column[:3].upper()}")
     else:
         umap_plot.opts(title=processing_parameters.wf_label_dictionary_wo_units[predictor_column])
 
@@ -905,8 +904,36 @@ for result, light, rig in itertools.product(results, lightings, rigs):
                                                      width=1.25, height=5, save=True)
         plt.close()
 
-    # --- Plot the fraction of multimodal tuned cells--- #
-    # TODO
+    # --- Plot the fraction of running modulated cells--- #
+    fixed_running_mod = data_dict[f'{session_types[0]}_{cell_kind}_running_modulated_cells']
+    frac_run_mod_fixed = fixed_running_mod.groupby(['mouse', 'day'])[['sig_run_modulated',
+                                                                       'sig_vis_run_modulated']].sum() / \
+                         fixed_running_mod.groupby(['mouse', 'day'])[['sig_run_modulated',
+                                                                       'sig_vis_run_modulated']].count()
+    rename_dict = dict(zip(list(frac_run_mod_fixed.columns), [col[4:] for col in frac_run_mod_fixed.columns]))
+    frac_run_mod_fixed = frac_run_mod_fixed.rename(columns=rename_dict)
+
+    for col in frac_run_mod_fixed.columns:
+        save_path = os.path.join(figure_save_path, f"sig_frac_{col}_{session_shorthand[0]}.png")
+        violinplot_fixed_run_mod_col = fp.violin_swarm(frac_run_mod_fixed.loc[:, [col]].copy(), save_path,
+                                                    cmap=fixed_violin_cmap, font_size='paper', backend='seaborn',
+                                                    width=1.5, height=5, save=True)
+        plt.close()
+
+    free_running_mod = data_dict[f'{session_types[1]}_{cell_kind}_running_modulated_cells']
+    frac_run_mod_free = free_running_mod.groupby(['mouse', 'day'])[['sig_run_modulated',
+                                                                       'sig_vis_run_modulated']].sum() / \
+                         free_running_mod.groupby(['mouse', 'day'])[['sig_run_modulated',
+                                                                       'sig_vis_run_modulated']].count()
+    rename_dict = dict(zip(list(frac_run_mod_free.columns), [col[4:] for col in frac_run_mod_free.columns]))
+    frac_run_mod_free = frac_run_mod_free.rename(columns=rename_dict)
+
+    for col in frac_run_mod_free.columns:
+        save_path = os.path.join(figure_save_path, f"sig_frac_{col}_{session_shorthand[1]}.png")
+        violinplot_free_run_mod_col = fp.violin_swarm(frac_run_mod_free.loc[:, [col]].copy(), save_path,
+                                                    cmap=free_violin_cmap, font_size='paper', backend='seaborn',
+                                                    width=1.5, height=5, save=True)
+        plt.close()
 
     # --- Plot the delta preferred orientation/direction, and delta DSI/OSI --- #
 
@@ -1110,7 +1137,7 @@ for result, light, rig in itertools.product(results, lightings, rigs):
                 umap_plot = create_umap_plot(plot_data, predictor_column)
 
                 save_name = os.path.join(figure_save_path, f"{cell_kind}_UMAP_{predictor_column}.png")
-                umap_plot = fp.save_figure(umap_plot, save_path=save_name, fig_width=6, dpi=800, fontsize='paper',
+                umap_plot = fp.save_figure(umap_plot, save_path=save_name, fig_width=3.5, dpi=800, fontsize='paper',
                                            target='save', display_factor=0.1)
 
         else:

snakemake_scripts/update_visual_tuning.py
@@ -9,7 +9,7 @@ import functions_bondjango as bd
 import functions_data_handling as fdh
 import functions_misc as fm
 import functions_tuning as tuning
-from snakemake_scripts.wf_tc_calculate import parse_kinematic_data, calculate_visual_tuning
+from snakemake_scripts.wf_tc_calculate import parse_kinematic_data, calculate_visual_tuning, predict_running_gmm_hmm
 
 if __name__ == '__main__':
     try:
@@ -65,18 +65,22 @@ if __name__ == '__main__':
     if len(raw_data) != 0:
 
         # --- Process visual tuning --- #
-        kinematics, raw_spikes, raw_fluor = parse_kinematic_data(raw_data[0], rig)
+        kinematics, inferred_spikes, deconvolved_fluor = parse_kinematic_data(raw_data[0], rig)
 
-        # Calculate dFF and normalize other neural data
+        # Calculate normalized fluorescence and spikes
         activity_ds_dict = {}
-        dff = tuning.calculate_dff(raw_fluor)
-        norm_spikes = tuning.normalize_responses(raw_spikes)
-        norm_fluor = tuning.normalize_responses(raw_fluor)
-        norm_dff = tuning.normalize_responses(dff)
-        activity_ds_dict['dff'] = dff
-        activity_ds_dict['norm_spikes'] = norm_spikes
-        activity_ds_dict['norm_fluor'] = norm_fluor
-        activity_ds_dict['norm_dff'] = norm_dff
+        activity_ds_dict['deconvolved_fluor'] = deconvolved_fluor
+        activity_ds_dict['inferred_spikes'] = inferred_spikes
+
+        norm_spikes = tuning.normalize_responses(inferred_spikes)
+        norm_fluor = tuning.normalize_responses(deconvolved_fluor)
+        activity_ds_dict['norm_deconvolved_fluor'] = norm_fluor
+        activity_ds_dict['norm_inferred_spikes'] = norm_spikes
+
+        # dff = tuning.calculate_dff(deconvolved_fluor, baseline_type='quantile', quantile=0.08)
+        # norm_dff = tuning.normalize_responses(dff)
+        # activity_ds_dict['dff'] = dff
+        # activity_ds_dict['norm_dff'] = norm_dff
 
         # Filter trials by head pitch if freely moving
         if rig in ['VTuningWF', 'VTuning']:
@@ -88,48 +92,64 @@ if __name__ == '__main__':
             viewed_trials = kinematics.groupby('trial_num').filter(
                 lambda x: (x['viewed'].sum() / len(x['viewed'])) > view_fraction).trial_num.unique()
 
-            raw_spikes_viewed = raw_spikes.loc[raw_spikes.trial_num.isin(viewed_trials)].copy()
-            norm_spikes_viewed = norm_spikes.loc[norm_spikes.trial_num.isin(viewed_trials)].copy()
-            norm_dff_viewed = norm_dff.loc[norm_dff.trial_num.isin(viewed_trials)].copy()
+            viewed_activity_dict = {}
+            for ds_key in activity_ds_dict.keys():
+                viewed_activity_dict[ds_key + '_viewed'] = activity_ds_dict[ds_key].loc[
+                    activity_ds_dict[ds_key].trial_num.isin(viewed_trials)].copy()
 
         else:
-            viewed_trials = raw_spikes.trial_num.unique()
-            raw_spikes_viewed = raw_spikes.copy()
-            norm_spikes_viewed = norm_spikes.copy()
-            norm_dff_viewed = norm_dff.copy()
+            viewed_trials = inferred_spikes.trial_num.unique()
 
-        activity_ds_dict['raw_spikes_viewed'] = raw_spikes_viewed
-        activity_ds_dict['norm_spikes_viewed'] = norm_spikes_viewed
-        activity_ds_dict['norm_dff_viewed'] = norm_dff_viewed
+            viewed_activity_dict = {}
+            for ds_key in activity_ds_dict.keys():
+                viewed_activity_dict[ds_key + '_viewed'] = activity_ds_dict[ds_key].copy()
+
+        activity_ds_dict.update(viewed_activity_dict)
 
         # Filter trials by running speed
         if rig == 'VTuningWF':
             speed_column = 'mouse_speed'
         else:
-            speed_column = 'wheel_speed'
+            speed_column = 'wheel_speed_abs'
 
-        speed_cutoff = np.percentile(np.abs(kinematics[speed_column]), 80)
-        kinematics['is_running'] = np.abs(kinematics[speed_column]) >= speed_cutoff
-        kinematics[f'{speed_column}_abs'] = np.abs(kinematics[speed_column])
+        # Use GMM - HMM to predict running state
+        running_prediction = predict_running_gmm_hmm(kinematics[speed_column].to_numpy().reshape(-1, 1),
+                                                     n_components=2)
+        running_idxs = np.argwhere(running_prediction > 0).flatten()
+        still_idxs = np.argwhere(running_prediction == 0).flatten()
+        kinematics['is_running'] = running_prediction > 0
 
-        still_trials = kinematics.groupby('trial_num').filter(
-            lambda x: x[f'{speed_column}_abs'].mean() < speed_cutoff).trial_num.unique()
+        still_trials = kinematics.iloc[still_idxs, :].groupby('trial_num').trial_num.unique()
         still_trials = viewed_trials[np.in1d(viewed_trials, still_trials)]
 
-        raw_spikes_viewed_still = raw_spikes_viewed.loc[raw_spikes_viewed.trial_num.isin(still_trials)]
-        norm_spikes_viewed_still = norm_spikes_viewed.loc[norm_spikes_viewed.trial_num.isin(still_trials)]
-        norm_dff_viewed_still = norm_dff_viewed.loc[norm_dff_viewed.trial_num.isin(still_trials)]
+        still_activity_dict = {}
+        for ds_key in viewed_activity_dict.keys():
+            still_activity_dict[ds_key + '_still'] = viewed_activity_dict[ds_key].loc[
+                viewed_activity_dict[ds_key].trial_num.isin(still_trials)].copy()
 
-        activity_ds_dict['raw_spikes_viewed_still'] = raw_spikes_viewed_still
-        activity_ds_dict['norm_spikes_viewed_still'] = norm_spikes_viewed_still
-        activity_ds_dict['norm_dff_viewed_still'] = norm_dff_viewed_still
+        activity_ds_dict.update(still_activity_dict)
 
         # Run the visual tuning loop and save to file
         print('Calculating visual tuning curves...')
-
+        vis_prop_dict = {}
         for ds_name in processing_parameters.activity_datasets:
-            activity_ds = activity_ds_dict[ds_name]
-            props = calculate_visual_tuning(activity_ds, bootstrap_shuffles=processing_parameters.bootstrap_repeats)
+
+            if ds_name not in activity_ds_dict.keys():
+                raise ValueError(f'Activity dataset {ds_name} not found in the dataset.')
+
+            if 'spikes' in ds_name:
+                activity_ds_type = 'spikes'
+            elif 'dff' in ds_name:
+                activity_ds_type = 'dff'
+            elif 'fluor' in ds_name:
+                activity_ds_type = 'fluor'
+            else:
+                raise ValueError(f'Unknown activity dataset type: {ds_name}')
+
+            activity_ds = activity_ds_dict[ds_name].copy()
+            props = calculate_visual_tuning(activity_ds, activity_ds_type,
+                                            metric_for_analysis=processing_parameters.analysis_metric,
+                                            bootstrap_shuffles=processing_parameters.bootstrap_repeats)
 
             # Update visual features to hdf5 files
             with pd.HDFStore(tc_file, mode='r+') as f:
@@ -139,6 +159,8 @@ if __name__ == '__main__':
                 # update the file
                 if feature_key in f.keys():
                     f.remove(feature)
+                    props.to_hdf(tc_file, feature)
+                else:
                     props.to_hdf(tc_file, feature)
 
         print(f'Updated visual tuning in {os.path.basename(tc_file)}')

wirefree_experiment.py
@@ -54,8 +54,8 @@ class WirefreeExperiment(DataContainer):
 
         # Neural and kinematic data structures
         self.kinematics = None
-        self.raw_spikes = None
-        self.raw_fluor = None
+        self.inferred_spikes = None
+        self.deconv_fluor = None
 
         # Tuning curve data structures
         self.visual_tcs = None
@@ -122,7 +122,7 @@ class WirefreeExperiment(DataContainer):
 
             self.cell_matches = self._parse_cell_matches(preproc['cell_matches'], self.preproc_info.result)
         
-            self._parse_kinematic_data(preproc['matched_calcium'])
+            self._parse_preprocessing(preproc['matched_calcium'])
             if full_kinem:
                 self.full_kinematics = preproc['full_traces']
 
@@ -130,7 +130,7 @@ class WirefreeExperiment(DataContainer):
             self.trial_set = preproc['trial_set']
             self.arena_corners = preproc['arena_corners']
 
-    def _parse_kinematic_data(self, matched_calcium):
+    def _parse_preprocessing(self, matched_calcium):
 
         if self.metadata.rig in ['VWheel', 'VWheelWF']:
             self.metadata.exp_type = 'fixed'
@@ -217,10 +217,10 @@ class WirefreeExperiment(DataContainer):
             self.kinematics['wheel_acceleration_abs'] = np.abs(self.kinematics['wheel_acceleration'].copy())
             self.kinematics['norm_wheel_speed'] = normalize(self.kinematics['wheel_speed_abs'])
 
-        self.raw_spikes = matched_calcium.loc[:, stimulus_cols + spikes_cols]
-        self.raw_spikes.columns = [key.rsplit('_', 1)[0] if 'spikes' in key else key for key in self.raw_spikes.columns]
-        self.raw_fluor = matched_calcium.loc[:, stimulus_cols + fluor_cols]
-        self.raw_fluor.columns = [key.rsplit('_', 1)[0] if 'fluor' in key else key for key in self.raw_fluor.columns]
+        self.inferred_spikes = matched_calcium.loc[:, stimulus_cols + spikes_cols]
+        self.inferred_spikes.columns = [key.rsplit('_', 1)[0] if 'spikes' in key else key for key in self.inferred_spikes.columns]
+        self.deconv_fluor = matched_calcium.loc[:, stimulus_cols + fluor_cols]
+        self.deconv_fluor.columns = [key.rsplit('_', 1)[0] if 'fluor' in key else key for key in self.deconv_fluor.columns]
 
     def _parse_params(self, df):
         params = df.to_dict('list')